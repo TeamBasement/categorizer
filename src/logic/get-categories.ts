af8253a9548e76b3d8fc6cb979afa1decbde683b
Tristan Rasmussen <tristanrasmussen@tristanrasmussen.com>
2024-10-14 12:20:06 -0400

    fix: fix type of category objects

--- src/logic/get-categories.ts
+++ src/logic/get-categories.ts
@@ -4,7 +4,7 @@ import { Category, CategoryObject } from "../types/category";
  * Gets categories for a set of objects.
  */
 export const getCategories = async (
-  objects: CategoryObject
+  objects: CategoryObject[]
 ): Promise<Category[]> => {
   const categoriesMap: Record<string, Category> = {};
 
